feadc71b2f6d07532beacec404cc14c0e8e206b9
nicklim011213 <nicklim011213@gmail.com>
2025-01-09 23:35:29 -0500

    Css Changes + Tailwind
    Im still deciding if I want to use tailwind
    I made a few components and some css changes to start off the page. Im thinking a blue nav bar followed by a banner image and title and then scrolling down to see some projects.

--- AOTW_Week5_Portfolio/src/App.vue
+++ AOTW_Week5_Portfolio/src/App.vue
@@ -1,85 +1,58 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectTile from './components/Project.vue'
+import './index.css'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <div id="TopBar"></div>
+    <h1> Current Projects:</h1>
+    <ul>
+      <li>
+      <ProjectTile></ProjectTile>
+      </li>
+      <li>
+      <ProjectTile></ProjectTile>
+      </li>
+      <li>
+      <ProjectTile></ProjectTile>
+      </li>
+    </ul>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+li
+{
+  list-style: none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+ul
+{
+  padding-left: 0%;
+
+  li:nth-child(even)
+  {
+    margin-left: 5%;
+    margin-top: 10%;
+  }
+
+  li:nth-child(odd)
+  {
+    margin-left: 85%;
+    margin-top: 10%;
+  }
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+#TopBar
+{
+  width: 100vw;
+  height: 5vh;
+  background-color: #00aeff;
+}
+
+h1
+{
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  align-self: center;
+  margin: 2%;
 }
 </style>
---
+++ AOTW_Week5_Portfolio/src/components/Project.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 id="ProjectTitle"> ProjectTitle</h1>
+</template>
+
+<style scoped>
+
+#ProjectTitle
+{
+    font-size: 2em;
+    text-align: left;
+}
+
+</style>
